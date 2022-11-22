ad4965e90a367ad9b904aae4174f3a3f21602054
Justin <justinjdbennett@gmail.com>
2022-11-22 23:32:55 +0000

    template literal to cleanup spinner

--- spinner2.js
+++ spinner2.js
@@ -1,11 +1,11 @@
-const spinStr = ['\r| ', '\r/ ', '\r- ', '\r\\ ', '\r| ', '\r/ ', '\r- ', '\r\\ ', '\r| '];
+const spinStr = '|/-\\|/-\\|';
 
 const spinner = str => {
   let timeout = 100;
   
   for (const char of str) {
     setTimeout(() => {
-      process.stdout.write(char);
+      process.stdout.write(`\r${char} `);
     }, timeout);
     timeout += 200;
   }
